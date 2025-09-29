687976bf699cd42eb1ceeeb61d8bae452a8803bd
Célio Eduardo <59587358+celio-eduardo@users.noreply.github.com>
2025-09-29 17:16:39 -0300

    Create movie.rb

---
+++ app/models/movie.rb
@@ -0,0 +1,6 @@
+# app/models/movie.rb
+# Esta classe representa um filme na nossa aplicação.
+# Ela herda de ApplicationRecord, que lhe dá toda a funcionalidade
+# para interagir com o banco de dados (graças ao Active Record).
+class Movie < ApplicationRecord
+end
